5b3cea775590e39964e69610d05f849ea5522ec8
906695962 <ledigin06@vt.edu>
2025-09-27 01:38:56 -0400

    added components and routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
+import Layout from './routes/Layout.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<App />} />
+
+
+
+      
+    </Route>
+  </Routes>
+</BrowserRouter>
 )
+
