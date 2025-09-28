3e2860873e183bf8492d177d060c62e08672939b
906695962 <ledigin06@vt.edu>
2025-09-27 22:12:38 -0400

    x

--- frontend/src/routes/UserForm.tsx
+++ frontend/src/routes/UserForm.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './UserForm.css';
-import { HorizontalFlagStrip } from '../components/FlagBorder';
 
 const countryFlags: { [key: string]: string } = {
     American: '🇺🇸',
